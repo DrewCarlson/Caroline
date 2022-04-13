60e5d3cc94f628056e255939619ea59ad41a9693
Drew Carlson <awcarlson@protonmail.com>
2022-04-13 09:08:05 -0700

    CarolineSDK builder tests

--- caroline-sdk/caroline-sdk-core/src/commonTest/kotlin/CarolineSDKTest.kt
+++ caroline-sdk/caroline-sdk-core/src/commonTest/kotlin/CarolineSDKTest.kt
@@ -8,12 +8,51 @@ import kotlin.test.assertTrue
 class CarolineSDKTest {
 
     @Test
+    fun testThrowsWithNoConfiguration() {
+        assertFailsWith<IllegalArgumentException> {
+            CarolineSDK {}
+        }
+    }
+
+    @Test
     fun testThrowsWithInvalidServerUrl() {
         val error = assertFailsWith<IllegalArgumentException> {
             CarolineSDK {
                 serverUrl = "test"
             }
         }
-        assertTrue(error.message.orEmpty().contains("serverUrl"))
+        assertTrue(
+            error.message.orEmpty().contains("serverUrl"),
+            "Exception should mention serverUrl"
+        )
+    }
+
+    @Test
+    fun testThrowsWithBlankProjectId() {
+        val error = assertFailsWith<IllegalArgumentException> {
+            CarolineSDK {
+                serverUrl = "https://test"
+                projectId = ""
+            }
+        }
+        assertTrue(
+            error.message.orEmpty().contains("projectId"),
+            "Exception should mention projectId"
+        )
+    }
+
+    @Test
+    fun testThrowsWithBlankApiKey() {
+        val error = assertFailsWith<IllegalArgumentException> {
+            CarolineSDK {
+                serverUrl = "https://test"
+                projectId = "abc"
+                apiKey = ""
+            }
+        }
+        assertTrue(
+            error.message.orEmpty().contains("apiKey"),
+            "Exception should mention apiKey"
+        )
     }
 }
