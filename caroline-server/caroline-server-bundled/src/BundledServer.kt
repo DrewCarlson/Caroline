2f0e51904a3a1aee8f36039a3e904e912a09404f
Drew Carlson <awcarlson@protonmail.com>
2022-04-12 22:31:42 -0700

    Bundled server cleanup

--- caroline-server/caroline-server-bundled/src/BundledServer.kt
+++ caroline-server/caroline-server-bundled/src/BundledServer.kt
@@ -2,11 +2,8 @@
 
 package cloud.caroline
 
-import io.ktor.http.CacheControl
-import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.http.content.CachingOptions
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.cachingheaders.*
@@ -15,8 +12,6 @@ import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.conditionalheaders.*
 import io.ktor.server.plugins.cors.*
 import io.ktor.server.plugins.forwardedheaders.*
-import io.ktor.server.plugins.partialcontent.*
-import io.ktor.server.request.*
 import org.slf4j.event.Level
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -24,24 +19,24 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(ConditionalHeaders)
+    install(ForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy
+    install(AutoHeadResponse)
+    install(CachingHeaders)
+
+    install(CallLogging) {
+        level = Level.INFO
+    }
+
     install(Compression) {
         gzip {
             priority = 1.0
         }
         deflate {
             priority = 10.0
-            minimumSize(1024) // condition
+            minimumSize(1024)
         }
     }
-
-    install(AutoHeadResponse)
-
-    install(CallLogging) {
-        level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
-    }
-
-    install(ConditionalHeaders)
 
     install(CORS) {
         methods.addAll(HttpMethod.DefaultMethods)
@@ -50,25 +45,5 @@ fun Application.module(testing: Boolean = false) {
         allowHeaders { true }
         allowHeader(HttpHeaders.Authorization)
         anyHost()
-    }
-
-    install(ForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy
-
-    install(CachingHeaders) {
-        options { _, outgoingContent ->
-            when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> {
-                    val max = CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60)
-                    CachingOptions(max)
-                }
-                else -> null
-            }
-        }
-    }
-
-    install(PartialContent) {
-        // Maximum number of ranges that will be accepted from a HTTP request.
-        // If the HTTP request specifies more ranges, they will all be merged into a single range.
-        maxRangeCount = 10
     }
 }
